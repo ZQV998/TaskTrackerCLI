cf282874dfd18030144f9d96b911a199cb960df7
刘兆坤 <610309220@qq.com>
2025-02-27 21:09:12 +0800

    创建文件目录：针对该项目，搭建实现框架

--- include/Task.h
+++ include/Task.h
@@ -1,6 +1,7 @@
 //
-// Created by ASUS on 2025/2/26.
+// Created by liuzk on 2025/2/27
 //
+// 该文件用于定义任务结构体和操作枚举类型
 
 #ifndef TASKTRACKERCLI_TASK_H
 #define TASKTRACKERCLI_TASK_H
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+//
+// Created by liuzk on 2025/2/27
+// 该文件为主文件，用于处理命令行参数，并调用其他文件进行操作
+
+#include <iostream>
+
+int main() {
+    std::cout << ""Hello, World!"" << std::endl;
+    return 0;
+}
